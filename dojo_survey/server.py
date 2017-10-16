2b2383b650a9dd0f0c9b08d90d2be7d2cf50f7fc
rrivas <rraph99@gmail.com>
2017-10-15 23:36:13 -0400

    Ninja Gold server and index file

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = 'very secret form'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/result', methods=["POST"])
+@app.route('/process', methods=["POST"])
 def result():
-    name = request.form['name']
-    location = request.form['location']
-    language = request.form['language']
-    comment = request.form['comment']
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!") # just pass a string to the flash function
+    elif len(request.form['location']) < 1:
+        flash("Location cannot be empty!") # just pass a string to the flash function
+    elif len(request.form['language']) < 1:
+        flash("Language cannot be empty!")
+    else:
+        flash("Success! Your name is {} at location {} and favorite language {}".format(request.form['name'], request.form['location'], request.form['language']))
+    return redirect('/')
+    # name = request.form['name']
+    # location = request.form['location']
+    # language = request.form['language']
+    # comment = request.form['comment']
 
-    return render_template('result.html', name=name, location=location, language=language, comment=comment)
+    # return render_template('result.html')
 
 app.run(debug=True)
