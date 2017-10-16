b476bdc7cd4fed888dd54b6d4c60edfaa5ce51b5
rrivas <rraph99@gmail.com>
2017-10-15 23:48:50 -0400

    Added flash message for form input

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -8,20 +8,18 @@ def index():
 
 @app.route('/process', methods=["POST"])
 def result():
-    if len(request.form['name']) < 1:
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    if len(name) < 1:
         flash("Name cannot be empty!") # just pass a string to the flash function
-    elif len(request.form['location']) < 1:
+    elif len(location) < 1:
         flash("Location cannot be empty!") # just pass a string to the flash function
-    elif len(request.form['language']) < 1:
+    elif len(language) < 1:
         flash("Language cannot be empty!")
     else:
         flash("Success! Your name is {} at location {} and favorite language {}".format(request.form['name'], request.form['location'], request.form['language']))
-    return redirect('/')
-    # name = request.form['name']
-    # location = request.form['location']
-    # language = request.form['language']
-    # comment = request.form['comment']
+    return render_template('/result.html', name=name, location=location, language=language)
 
-    # return render_template('result.html')
 
 app.run(debug=True)
