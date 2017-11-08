46e4eee7b4da6a6909e4a30f6585680c42bb8e15
rrivas <rraph99@gmail.com>
2017-11-07 22:03:37 -0500

    math script

--- python_oop/math.py
+++ python_oop/math.py
@@ -22,30 +22,3 @@ class MathDojo(object):
 
 md = MathDojo()
 print(md.add([1,2,5,7]).subtract(1).result)
-
-# class Dog(Animal):
-#     def __init__(self, name):
-#         super(Dog, self).__init__(name)
-#         self.health = 150
-#     def pet(self):
-#         self.health += 5
-#         return self
-#
-# dog = Dog("Rambo")
-# dog.walk().walk().walk().run().run().pet().displayHealth()
-#
-# class Dragon(Animal):
-#     def __init__(self, name):
-#         super(Dragon, self).__init__(name, health)    # use super to call the Human __init__ method
-#         self.health = 10                # every Ninja starts off with 10 stealth
-#     def fly(self):
-#         self.health -= 10
-#     def displayHealth(self):
-#         print str(self.health) + " I am a dragon"
-#
-# dragon1 = Dragon('tiantian', 5).displayHealth()
-
-#
-# dog1 = Dog("tiantian")
-# dog1.displayHealth()
-# Dog.walk().walk().walk().run().run().pet().displayHealth()
